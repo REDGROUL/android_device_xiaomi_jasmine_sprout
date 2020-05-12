3d91080c8ebcec77917b1d83f9595b1ffe3d1825
PradyX <pradyumnbhushan@gmail.com>
2020-05-12 18:19:34 +0530

    jasmine: lucid bringup

--- lucid_jasmine_sprout.mk
+++ lucid_jasmine_sprout.mk
@@ -30,9 +30,6 @@ $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Official-ify
-DERP_BUILDTYPE := Official
-
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
@@ -40,6 +37,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Device identifier
 PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := derp_jasmine_sprout
+PRODUCT_NAME := lucid_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
