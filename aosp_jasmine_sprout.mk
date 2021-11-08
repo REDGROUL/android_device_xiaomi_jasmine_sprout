0146f8bd8eff5c2d453391e6b06b828bd90724fb
REDGROUL <38361748+REDGROUL@users.noreply.github.com>
2021-11-08 15:00:58 +0300

    aex-ify

--- aosp_jasmine_sprout.mk
+++ aosp_jasmine_sprout.mk
@@ -25,21 +25,23 @@
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit some common aosip stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Official-ify
-DERP_BUILDTYPE := Official
 
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
 
 # Device identifier
+
 PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := derp_jasmine_sprout
+PRODUCT_NAME := aosp_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
+
+# TWRP Support
+BUILD_TWRP := true
